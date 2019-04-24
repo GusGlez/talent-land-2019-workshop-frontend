4d78dba149b8168104437f36c3ad5f195230d2b3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-24 00:04:36 +0000

    Deploy GusGlez/talent-land-2019-workshop-frontend to github.com/GusGlez/talent-land-2019-workshop-frontend.git:gh-pages

---
+++ precache-manifest.5fee156c79643ceaec83aaa47102a8bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "73065f93163d6feb5fa3",
+    "url": "/static/js/main.998963d3.chunk.js"
+  },
+  {
+    "revision": "ed2eb8a31b3d3fe961a8",
+    "url": "/static/js/2.1efa75b7.chunk.js"
+  },
+  {
+    "revision": "73065f93163d6feb5fa3",
+    "url": "/static/css/main.494ca870.chunk.css"
+  },
+  {
+    "revision": "bcaaa989a79e692bb72fbd8aa9cf7ea8",
+    "url": "/index.html"
+  }
+];
